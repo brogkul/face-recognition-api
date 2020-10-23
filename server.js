6f5703161d3e13eeb17608caeda664472deec9ee
Anthony Melean <brogkul@gmail.com>
2020-10-23 17:59:33 -0400

    Adding database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'brogkul',
-    password : '',
-    database : 'face_recognition'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
