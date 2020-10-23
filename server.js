c4dc6d72c0bcb9ee66fee6f385f9f79c13ea12c7
Anthony Melean <brogkul@gmail.com>
2020-10-23 11:45:59 -0400

    Adding root

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cors());
 //   res.send(database.users)
 // })
 
+app.get('/', (req, res) => {res.send('working')})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
