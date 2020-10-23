076b092c8d817d6efcc7f8a1650e23a42a5f471b
Anthony Melean <brogkul@gmail.com>
2020-10-23 11:40:31 -0400

    Fixing port

--- server.js
+++ server.js
@@ -28,14 +28,11 @@ app.use(cors());
 // })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
-
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
-
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApi(req, res)})
 
-app.listen(3000, () => {
-  console.log('app running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app running on port ${process.env.PORT}`)
 })
