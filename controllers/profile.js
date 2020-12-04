e7d042e28b1ea1c6a0ad40e51f8985e3273d9397
Anthony Melean <brogkul@gmail.com>
2020-12-04 17:58:58 -0400

    Adding Authentication

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,15 +1,34 @@
 const handleProfile = (req, res, db) => {
-  const {id} = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not Found')
-      }
-    })
-}
+	const { id } = req.params;
+	db.select('*')
+		.from('users')
+		.where({ id })
+		.then((user) => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('Not Found');
+			}
+		});
+};
+
+const handleProfileUpdate = (req, res, db) => {
+	const { id } = req.params;
+	const { name, age, pet } = req.body.formInput;
+	db('users')
+		.where({ id })
+		.update({ name })
+		.then((resp) => {
+			if (resp) {
+				res.json('success');
+			} else {
+				res.status(400).json('Unable to update');
+			}
+		})
+		.catch((err) => res.status(400).json('error updating user'));
+};
 
 module.exports = {
-  handleProfile
+	handleProfile,
+	handleProfileUpdate,
 };
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
 const db = knex({
 	client: 'pg',
@@ -23,19 +24,20 @@ app.use(morgan('combined'));
 app.get('/', (req, res) => {
 	res.send('working');
 });
-app.post('/signin', (req, res) => {
-	signin.handleSignin(req, res, db, bcrypt);
-});
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 app.post('/register', (req, res) => {
 	register.handleRegister(req, res, db, bcrypt);
 });
-app.get('/profile/:id', (req, res) => {
+app.get('/profile/:id', auth.requireAuth, (req, res) => {
 	profile.handleProfile(req, res, db);
 });
-app.put('/image', (req, res) => {
+app.post('/profile/:id', auth.requireAuth, (req, res) => {
+	profile.handleProfileUpdate(req, res, db);
+});
+app.put('/image', auth.requireAuth, (req, res) => {
 	image.handleImage(req, res, db);
 });
-app.post('/imageurl', (req, res) => {
+app.post('/imageurl', auth.requireAuth, (req, res) => {
 	image.handleApi(req, res);
 });
 
